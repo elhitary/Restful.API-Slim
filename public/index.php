77c896e79d6bf88e455a45aed03773caff3011f2
Farouk El hitary <fel-hita@e1r3p15.1337.ma>
2019-06-14 20:55:47 +0100

    Updated Configuration file by adding database credentials and other info

--- public/index.php
+++ public/index.php
@@ -1,3 +1,23 @@
 <?php
-phpinfo();
-?>
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require '../vendor/autoload.php';
+// config array
+
+$config['displayErrorDetails'] = true;
+$config['addContentLengthHeader'] = false;
+
+$config['db']['host'] = 'localhost';
+$config['db']['user'] = 'root';
+$config['db']['pass'] = 'root';
+$config['db']['dbname'] = 'myDB';
+
+$app = new \Slim\App(['settings' => $config]);
+$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+	    $name = $args['name'];
+		    $response->getBody()->write("Hello, $name");
+
+		    return $response;
+});
+$app->run();
