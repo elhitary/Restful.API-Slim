46ca2e213fdd3409b7057cc9dda372c252f33e2e
Farouk El hitary <fel-hita@e1r3p15.1337.ma>
2019-06-15 21:09:52 +0100

    Added new api route  /api/customer/add which adds a new customer into the database

--- public/customers.php
+++ public/customers.php
@@ -46,3 +46,41 @@ $app->get('/api/customer/{id}', function(Request $request, Response $response){
 		echo '{"error": {"text": '.$e->getMessage().'}';
 	}
 });
+
+// add customer
+
+$app->post('/api/customer/add', function(Request $request, Response $response){
+
+	$username = $request->getParam('username');
+	$first_name = $request->getParam('first_name');
+    $last_name = $request->getParam('last_name');
+    $gender = $request->getParam('gender');
+    $password = $request->getParam('password');
+    $status = $request->getParam('status');
+
+	$sql = "INSERT INTO customers (username,first_name,last_name,gender,password,status) VALUES
+	(:username,:first_name,:last_name,:gender, :password, :status)";
+
+	try{
+		// Get DB Object
+		$db = new db();
+		//connect
+		$db = $db->connect();
+
+		$stmt = $db->prepare($sql);
+
+		$stmt->bindParam(':username', 	$username);
+		$stmt->bindParam(':first_name', $first_name);
+		$stmt->bindParam(':last_name', 	$last_name);
+		$stmt->bindParam(':gender', 	$gender);
+		$stmt->bindParam(':password', 	$password);
+		$stmt->bindParam(':status', 	$status);
+
+		$stmt->execute();
+
+		echo '{"notice": {"text": "Customer Added"}';
+
+	} catch(PDOException $e){
+		echo '{"error": {"text": '.$e->getMessage().'}';
+	}
+});
