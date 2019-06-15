487b2d1b67662613ded5d45717f2662e83467892
Farouk El hitary <fel-hita@e1r3p15.1337.ma>
2019-06-15 20:30:25 +0100

    Added new api route L /api/customer/{id} which list specific customer by a given id

--- public/customers.php
+++ public/customers.php
@@ -22,4 +22,27 @@ $app->get('/api/customers', function(Request $request, Response $response){
 	} catch(PDOexception $e){
 		echo '{"error": {"text": '.$e->getMessage().'}';
 	}
+
+});
+// get specific customers
+
+$app->get('/api/customer/{id}', function(Request $request, Response $response){
+
+	$user_id = $request->getAttribute('id');
+
+	$sql = "SELECT * FROM customers WHERE user_id = $user_id";
+
+	try{
+		// Get DB Object
+		$db = new db();
+		//connect
+		$db = $db->connect();
+
+		$stmt = $db->query($sql);
+		$customer = $stmt->fetchAll(PDO::FETCH_OBJ);
+		$db = null;
+		echo json_encode($customer);
+	} catch(PDOexception $e){
+		echo '{"error": {"text": '.$e->getMessage().'}';
+	}
 });
